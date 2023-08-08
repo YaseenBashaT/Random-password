0e1096c9a85537d60285ac153722204b494a8cd1
Thippaluri Yaseen Basha <yasenroycey6@gmail.com>
2023-08-08 19:47:17 +0530

    Add files via upload

---
+++ Random Password/index.js
@@ -0,0 +1,30 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+function randomChar() {
+    let randchar = Math.floor(Math.random()*characters.length)
+    return characters[randchar]
+}
+let firstPass=""
+let secondPass = ""
+function passGen(){
+    firstPass.textContent=""
+    secondPass.textContent =""
+    passGen1()
+    passGen2()
+    function passGen1(){
+        
+        firstPass = document.getElementById("pass1")
+            for(let i=0; i<13; i++){
+                firstPass.textContent += randomChar() 
+
+            }
+    }
+
+    function passGen2(){
+        
+        secondPass =document.getElementById("pass2")
+            for(let i=0; i<13; i++){
+                secondPass.textContent += randomChar() 
+            }
+    }
+}
